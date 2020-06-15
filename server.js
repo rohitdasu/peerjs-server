cd2a9acfdc2a6f8e04a46f4b8d51c61398e9a81f
Rohit <dasurohit123@gmail.com>
2020-06-15 15:02:15 +0530

    push

--- server.js
+++ server.js
@@ -1,32 +1,16 @@
-const { PeerServer } = require("peer");
-const Express = require("express");
-const BodyParser = require("body-parser");
-const multer = require("multer");
+const express = require("express");
+const { ExpressPeerServer } = require("peer");
 
-var app = Express();
-var cors = require("cors");
+const app = express();
 
-const fs = require("fs");
+app.get("/", (req, res, next) => res.send("Hello world!"));
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-app.use(cors());
-app.use(BodyParser.json());
-app.use(BodyParser.urlencoded({ extended: true }));
+// =======
 
-app.get("/", (req, res) => {
-  res.send("Hi, This is the back-end");
+const server = app.listen(process.env.PORT || 9000);
+
+const peerServer = ExpressPeerServer(server, {
+  path: "/myapp",
 });
 
-
-const peerServer = PeerServer({ port: 9000, path: "/mycommunication" });
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Server Started!!!");
-});
+app.use("/peerjs", peerServer);
