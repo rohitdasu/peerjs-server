1d1a0479fcb101ffa547a602f6fbdae65d34cba9
Rohit <dasurohit123@gmail.com>
2020-11-18 23:52:26 +0530

    push

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { ExpressPeerServer } = require("peer");
+const cors = require('cors');
 
 const app = express();
 
@@ -7,10 +8,15 @@ app.get("/", (req, res, next) => res.send("Hello world!"));
 
 // =======
 
-const server = app.listen(process.env.PORT || 9000);
+let port = process.env.PORT || 9000;
+
+console.log(port);
+
+const server = app.listen(port);
 
 const peerServer = ExpressPeerServer(server, {
   path: "/myapp",
 });
 
 app.use("/peerjs", peerServer);
+app.use(cors())
